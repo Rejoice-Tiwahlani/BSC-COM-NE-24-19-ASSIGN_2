dfda3382e351bc5db2315695c80552cc373075ad
Rejoice-Tiwahlani <bsc-com-ne-24-19@unima.ac.mw>
2022-03-18 13:09:38 +0200

    More and more commits

--- MyHouseLights.java
+++ MyHouseLights.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -43,47 +45,62 @@ public class MyHouseLights extends Application{
         pane.getChildren().addAll(file, edit, format, view, help);
 
 
-        //first room
+        //The first room
         Rectangle room1 = new Rectangle(100,120);
         room1.setFill(Color.YELLOW);
         room1.setStroke(Color.BLACK);
         room1.setX(90);
         room1.setY(60);
-        pane.getChildren().add(room1);
+        Label room1Label = new Label("Room 1");
+        room1Label.setLayoutX(140);
+        room1Label.setLayoutY(80);
+        pane.getChildren().addAll(room1,room1Label);
 
-        //second room
+        //The second room
         Rectangle room2 = new Rectangle(120,40);
         room2.setFill(Color.GREEN);
         room2.setStroke(Color.BLACK);
         room2.setX(190);
         room2.setY(125);
-        pane.getChildren().add(room2);
+        Label room2Label = new Label("Room 2");
+        room2Label.setLayoutX(260);
+        room2Label.setLayoutY(150);
+        pane.getChildren().addAll(room2, room2Label);
 
-        //third room
+        //The third room
         Rectangle room3 = new Rectangle(60,85);
         room3.setFill(Color.ORANGE);
         room3.setStroke(Color.BLACK);
         room3.setX(190);
         room3.setY(60);
-        pane.getChildren().add(room3);
+        Label room3Label = new Label("Room 3");
+        room3Label.setLayoutX(200);
+        room3Label.setLayoutY(100);
+        pane.getChildren().addAll(room3, room3Label);
 
-        //fourth room
+        // The fourth room
         Rectangle room4 = new Rectangle(60,85);
         room4.setFill(Color.ORANGE);
         room4.setStroke(Color.BLACK);
         room4.setX(250);
         room4.setY(60);
-        pane.getChildren().add(room4);
+        Label room4Label = new Label("Room 4");
+        room4Label.setLayoutX(260);
+        room4Label.setLayoutY(120);
+        pane.getChildren().addAll(room4, room4Label);
 
-        //fifth room
+        // The fifth room
         Rectangle room5 = new Rectangle(80,105);
         room5.setFill(Color.YELLOW);
         room5.setStroke(Color.BLACK);
         room5.setX(310);
         room5.setY(60);
-        pane.getChildren().add(room5);
+        Label room5Label = new Label("Room 5");
+        room5Label.setLayoutX(320);
+        room5Label.setLayoutY(110);
+        pane.getChildren().addAll(room5, room5Label);
 
-        //sixth room
+        // The sixth room
         Rectangle room6 = new Rectangle(90,110);
        room6.setFill(Color.RED);
        room6.setStroke(Color.BLACK);
@@ -91,13 +108,35 @@ public class MyHouseLights extends Application{
        room6.setY(165);
        pane.getChildren().add(room6);
 
-       //seventh and last room
+       //The seventh and last room
        Rectangle room7 = new Rectangle(110,110);
        room7.setFill(Color.YELLOW);
        room7.setStroke(Color.BLACK);
        room7.setX(280);
        room7.setY(165);
        pane.getChildren().add(room7);
+
+       //Adding the buttons to the window
+       // button 1
+       Button buttonRoomAll = new Button("AllRooms ON/OFF");
+        buttonRoomAll.setPrefSize(120, 20);
+        buttonRoomAll.setLayoutY(400);
+        buttonRoomAll.setLayoutX(130);
+        pane.getChildren().add(buttonRoomAll);
+
+        //button 2
+        Button buttonRoom2 = new Button("Room 2");
+        buttonRoom2.setPrefSize(120, 20);
+        buttonRoom2.setLayoutY(400);
+        buttonRoom2.setLayoutX(260);
+        pane.getChildren().add(buttonRoom2);
+
+        //button 3
+        Button buttonRoom1and2 = new Button("Room 1&2");
+        buttonRoom1and2.setPrefSize(120, 20);
+        buttonRoom1and2.setLayoutY(440);
+        buttonRoom1and2.setLayoutX(130);
+        pane.getChildren().add(buttonRoom1and2);
 
 
 
