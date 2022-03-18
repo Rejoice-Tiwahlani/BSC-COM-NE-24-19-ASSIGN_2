d76abc3ff9bb9dee28fe4376d23dd826150cb1c9
Rejoice-Tiwahlani <bsc-com-ne-24-19@unima.ac.mw>
2022-03-18 12:05:20 +0200

    More incoming commkits

--- MyHouseLights.java
+++ MyHouseLights.java
@@ -42,6 +42,8 @@ public class MyHouseLights extends Application{
 
         pane.getChildren().addAll(file, edit, format, view, help);
 
+
+        //first room
         Rectangle room1 = new Rectangle(100,120);
         room1.setFill(Color.YELLOW);
         room1.setStroke(Color.BLACK);
@@ -49,7 +51,55 @@ public class MyHouseLights extends Application{
         room1.setY(60);
         pane.getChildren().add(room1);
 
-        
+        //second room
+        Rectangle room2 = new Rectangle(120,40);
+        room2.setFill(Color.GREEN);
+        room2.setStroke(Color.BLACK);
+        room2.setX(190);
+        room2.setY(125);
+        pane.getChildren().add(room2);
+
+        //third room
+        Rectangle room3 = new Rectangle(60,85);
+        room3.setFill(Color.ORANGE);
+        room3.setStroke(Color.BLACK);
+        room3.setX(190);
+        room3.setY(60);
+        pane.getChildren().add(room3);
+
+        //fourth room
+        Rectangle room4 = new Rectangle(60,85);
+        room4.setFill(Color.ORANGE);
+        room4.setStroke(Color.BLACK);
+        room4.setX(250);
+        room4.setY(60);
+        pane.getChildren().add(room4);
+
+        //fifth room
+        Rectangle room5 = new Rectangle(80,105);
+        room5.setFill(Color.YELLOW);
+        room5.setStroke(Color.BLACK);
+        room5.setX(310);
+        room5.setY(60);
+        pane.getChildren().add(room5);
+
+        //sixth room
+        Rectangle room6 = new Rectangle(90,110);
+       room6.setFill(Color.RED);
+       room6.setStroke(Color.BLACK);
+       room6.setX(190);
+       room6.setY(165);
+       pane.getChildren().add(room6);
+
+       //seventh and last room
+       Rectangle room7 = new Rectangle(110,110);
+       room7.setFill(Color.YELLOW);
+       room7.setStroke(Color.BLACK);
+       room7.setX(280);
+       room7.setY(165);
+       pane.getChildren().add(room7);
+
+
 
         Scene scene = new Scene(pane,600,600);
         primaryStage.setTitle("My House Lights");
